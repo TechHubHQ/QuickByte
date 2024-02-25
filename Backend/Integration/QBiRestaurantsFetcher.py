1fca0e67308f80ea775405ce9b71cf7bb1db0566
Kalyan Kanuri <kalyankanuri497@gmail.com>
2024-02-25 09:18:47 +0530

    Introducing whole new Rebuild process to DB

--- Backend/Integration/QBiRestaurantsFetcher.py
+++ Backend/Integration/QBiRestaurantsFetcher.py
@@ -1,36 +1,60 @@
+import os
+import sys
 import requests
+# Add the root directory to the Python path
+root_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(root_dir)
+print(f"Root directory added to Python path: {root_dir}")
 from app import app
+from dotenv import load_dotenv
 from Backend.Models.QBmLoadRestaurantsByID import CreateRestaurant, RestaurantsByLoc
 from Backend.Models.QBmLoadLocationID import CityLocation
 from Backend.Connections.QBcDBConnector import db
 
+script_dir = os.path.dirname(__file__)
+env_path = os.path.join(script_dir, '..', '..', 'config', '.env')
+
+load_dotenv(env_path)
 app.app_context().push()
 db.create_all()
 
-url = "https://travel-advisor.p.rapidapi.com/restaurants/list"
+
+url = os.environ.get('REST_API')
 
 headers = {
-    "X-RapidAPI-Key": "897b3eccc5msh07c09ba21aae956p1040b7jsnbdaeb583f509",
-    "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com"
+    "X-RapidAPI-Key": os.environ.get('REST_API_KEY'),
+    "X-RapidAPI-Host": os.environ.get('REST_API_HOST')
 }
 
 city_ids = CityLocation.query.with_entities(CityLocation.loc_id).distinct().all()
 loc_ids = [city_id[0] for city_id in city_ids]
+print(loc_ids)
 
 for loc_id in loc_ids:
+    print(loc_id)
     querystring = {"location_id": loc_id}
     response = requests.get(url, headers=headers, params=querystring)
     tmpResArr = response.json().get('data', [])
 
+
     for res in tmpResArr:
-        res_flag = RestaurantsByLoc.query.filter_by(res_flag=None).first()
-        if not res_flag:
+        # Check if the restaurant already exists in the database
+        existing_restaurant = RestaurantsByLoc.query.filter_by(location_id=loc_id).first()
+        if existing_restaurant:
+            print(f"Restaurant already exists in the database: loc_id --> {res['location_id']}")
+            continue  # Skip this restaurant if it already exists in the database
+
+        res_flag = None  # Initialize res_flag to None
+
+        # Check if res_flag is NULL or 0
+        if res_flag is None or res_flag == 0:
+            print(res_flag)
             if None in (
-                    res.get('location_id'), res.get('name'), res.get('num_reviews'), res.get('timezone'),
-                    res.get('rating'),
-                    res.get('ranking'), res.get('web_url'), res.get('phone'), res.get('email')):
+                    res.get('location_id'), res.get('name')):
+                print(f"Skipping this restaurant {res.get('location_id')} or {res.get('name')} field is None")
                 continue  # Skip this restaurant if any required field is None
-            print("\nNew Restaurant -- registration")
+
+            print("\nNew Restaurant -- registration: ")
             print(res.get('location_id'))
             print(res.get('name'))
             print(res.get('num_reviews'))
@@ -52,7 +76,6 @@ for loc_id in loc_ids:
                 full_address = 'Unknown Address'
             print(full_address)
 
-            # Now you can insert this data into the database
             location_id = res.get('location_id')
             restaurant_name = res.get('name')
             num_reviews = res.get('num_reviews')

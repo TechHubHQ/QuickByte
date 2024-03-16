96aeeeda3373a99345b4d86fb8ed700357d02e85
Kalyan Kanuri <kalyankanuri497@gmail.com>
2024-03-16 18:50:14 +0530

    use file_path instead of baseFilename

--- Config/PyLogger.py
+++ Config/PyLogger.py
@@ -15,16 +15,17 @@ class RollingFileHandler(logging.FileHandler):
         self.log_dir = log_dir
         self.log_file_name = log_file_name
         self.current_date = datetime.now().date()
-        self.baseFilename = self.get_log_file_path()
-        super().__init__(self.baseFilename, mode, encoding, delay)
-        super().__init__(self.get_log_file_path(), mode, encoding, delay)
+        self.file_path = self.get_log_file_path()  # Initialize file_path
+        super().__init__(self.file_path, mode, encoding, delay)
 
     def get_log_file_path(self):
         current_date = datetime.now().date()
         if current_date != self.current_date:
             self.current_date = current_date
-            self.baseFilename = os.path.join(self.log_dir, f'{current_date:%Y-%m-%d}_{self.log_file_name}')
-        return self.baseFilename
+            file_path = os.path.join(self.log_dir, f'{current_date:%Y-%m-%d}_{self.log_file_name}')
+        else:
+            file_path = os.path.join(self.log_dir, f'{self.current_date:%Y-%m-%d}_{self.log_file_name}')
+        return file_path
 
     def emit(self, record):
         self.stream = self._open()

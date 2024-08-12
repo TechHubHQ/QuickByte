6ff07760b5ce5f89a0f037b917c0c85c36139926
Kalyan.Kanuri <kalyankanuri497@gmail.com>
2024-08-13 05:13:53 +0530

    convert to react tsx + go lang for QB 2.0

---
+++ Backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/TechHubHQ/QuickByte
+
+go 1.22.6
--- Frontend/Static/js/Navbar.js
+++
@@ -1,5 +0,0 @@
-function toggleMenu() {
-    var navLinks = document.getElementById("navLinks");
-    navLinks.classList.toggle("show");
-  }
-  

fe56e9a8bfdffe6f2f9203b7492ea67a486cf7e4
Harish Vaddadhi <vharish35063@gmail.com>
2024-09-16 15:29:18 +0530

    add restaurant details

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/Landing";
 import LoginPage from "./pages/Login";
 import SignUpPage from "./pages/SignUp";
+import Home from "./pages/Home";
 
 const App: React.FC = () => {
   return (
@@ -11,6 +12,8 @@ const App: React.FC = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/home" element={<Home />} />
+
       </Routes>
     </Router>
   );

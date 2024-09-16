45754ebf7608de20db1e32ca45157cd859a770bd
Kalyan.Kanuri <kalyankanuri497@gmail.com>
2024-09-16 18:30:47 +0530

    enable supabase for prod

--- Backend/database/connect_db.go
+++ Backend/database/connect_db.go
@@ -16,11 +16,11 @@ func ConnectDB() (*sql.DB, error) {
 		return nil, err
 	}
 
-	DB_HOST 			:= os.Getenv("DB_HOST")
-	DB_PORT				:= os.Getenv("DB_PORT")
-	DB_USER				:= os.Getenv("DB_USER")
-	DB_PASSWORD		:= os.Getenv("DB_PASSWORD")
-	DB_NAME				:= os.Getenv("DB_NAME")
+	DB_HOST 			:= os.Getenv("SBPG_HOST")
+	DB_PORT				:= os.Getenv("SBPG_PORT")
+	DB_USER				:= os.Getenv("SBPG_USER")
+	DB_PASSWORD		:= os.Getenv("SBPG_PASSWORD")
+	DB_NAME				:= os.Getenv("SBPG_NAME")
 
 	conn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", 
 												DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME)

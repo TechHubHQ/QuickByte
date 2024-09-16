8ba959f7b9a845fc4638847bd8885408f5da3c44
Kalyan.Kanuri <kalyankanuri497@gmail.com>
2024-09-16 21:38:03 +0530

    fix connection to local PG instead of SupaBase

--- Backend/database/dev/connect_db.go
+++ Backend/database/dev/connect_db.go
@@ -1,10 +1,11 @@
 package database
 
 import (
-	"os"
-	"log"
-	"fmt"
 	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
@@ -16,15 +17,15 @@ func ConnectDB() (*sql.DB, error) {
 		return nil, err
 	}
 
-	DB_HOST 			:= os.Getenv("SBPG_HOST")
-	DB_PORT				:= os.Getenv("SBPG_PORT")
-	DB_USER				:= os.Getenv("SBPG_USER")
-	DB_PASSWORD		:= os.Getenv("SBPG_PASSWORD")
-	DB_NAME				:= os.Getenv("SBPG_NAME")
+	DB_HOST := os.Getenv("DB_HOST")
+	DB_PORT := os.Getenv("DB_PORT")
+	DB_USER := os.Getenv("DB_USER")
+	DB_PASSWORD := os.Getenv("DB_PASSWORD")
+	DB_NAME := os.Getenv("DB_NAME")
 
-	conn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", 
-												DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME)
-												
+	conn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME)
+
 	db, err := sql.Open("postgres", conn)
 
 	if err != nil {

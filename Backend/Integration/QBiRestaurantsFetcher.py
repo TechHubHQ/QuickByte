2ebf8315d756be43b5c6743cb2e772be2eacaca7
Kalyan.Kanuri <kalyankanuri497@gmail.com>
2024-10-12 16:19:47 +0530

    Fix typo: redirect restaurants logs to RestaurantsFetcher

--- Backend/Integration/QBiRestaurantsFetcher.py
+++ Backend/Integration/QBiRestaurantsFetcher.py
@@ -47,7 +47,7 @@ current_date = datetime.now().strftime('%Y-%m-%d')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-file_handler = RollingFileHandler(INTEGRATION_LOG_DIR, 'MenuFetcher.log')
+file_handler = RollingFileHandler(INTEGRATION_LOG_DIR, 'RestaurantsFetcher.log')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 

fdfbbfa6fc5c80537d8282d63c40cd1593444354
kalyankanuri497 <kalyankanuri497@gmail.com>
2024-11-13 06:17:01 +0000

    add additional pkgs to nix env

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,10 +7,13 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.go
-    # pkgs.python311
+    pkgs.fontconfig.bin
+    pkgs.nano
+    pkgs.oh-my-posh
+    pkgs.python311
     # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.nodejs_20
+    pkgs.nodePackages.nodemon
   ];
 
   # Sets environment variables in the workspace
